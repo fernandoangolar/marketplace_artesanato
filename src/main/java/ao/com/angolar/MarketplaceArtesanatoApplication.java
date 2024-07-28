bb49b44f22c419e5d9e4aff4392ac36ba40022d2
Fernando Angolar <fernandoangolar581@gmail.com>
2024-07-29 00:26:52 +0100

    inicio do projeto

---
+++ src/main/java/ao/com/angolar/MarketplaceArtesanatoApplication.java
@@ -0,0 +1,13 @@
+package ao.com.angolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceArtesanatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceArtesanatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/com/angolar/MarketplaceArtesanatoApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.angolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceArtesanatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
